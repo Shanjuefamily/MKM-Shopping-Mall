6e85ec45075fd0946d9b690ff7cd5112c68f833b
Sai Myo Min Aung <saimyominaung2110@gmail.com>
2023-04-20 13:19:46 +0630

    first commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,27 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
+ */
+
+import './bootstrap';
+import "../css/app.css";
+
+/**
+ * Next, we will create a fresh React component instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+import ReactDOM from "react-dom/client";
+import React from 'react';
+import FrontendLayout from './components/FrontendLayout';
+
+if (document.getElementById("app")) {
+    const Index = ReactDOM.createRoot(document.getElementById("app"));
+
+    Index.render(
+        <React.StrictMode>
+            <FrontendLayout />
+        </React.StrictMode>
+    );
+}
---
+++ resources/js/components/FrontendLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function Example() {
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+}
+
+export default Example;
